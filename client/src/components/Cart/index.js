a28321635140504843c3d69dd0428702abaeaa1d
astro <angelica.strong0725@gmail.com>
2024-02-17 14:52:36 -0800

    rm extra line

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -13,14 +13,11 @@ import { useDispatch, useSelector } from "react-redux";
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
-
-
 const Cart = () => {
  // const [state, dispatch] = useStoreContext();
 
- const dispatch = useDispatch();
- const state = useSelector((state) => state);
- 
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
